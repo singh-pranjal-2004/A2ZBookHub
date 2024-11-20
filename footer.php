813e8efcc9c977b30844ab3adbc6f3b9ca2999fb
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-11-20 15:29:16 +0530

    Add files via upload
    Final Commit

---
+++ footer.php
@@ -0,0 +1,32 @@
+<section class="footer" style="background-color: rgba(0, 0, 0, 0.799);">
+  <div class="footer_box_container">
+    <div class="footer_box">
+      <div class="footer_logo_cont">
+        <img src="book_logo.png" alt="">
+      <a href="home.php" class="book_logo">A2Z Book Hub</a>
+
+      </div>
+      <p><i class="fas fa-phone"></i> +91 93198 30528</p>
+      <p><i class="fas fa-envelope"></i> a2zbookhub@gmail.com</p>
+      <p><i class="fas fa-map-marker-alt"></i> Bhubaneswar, India - 751024</p>
+      <p><i class="fa-solid fa-shop"></i> Shop Timings : 9am - 9pm</p>
+    </div>
+
+    <div class="footer_box">
+      <h3>Quick Links</h3>
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="shop.php">Shop</a>
+      <a href="contact.php">Contact</a>
+    </div>
+
+    <div class="footer_box">
+      <h3>Other Links</h3>
+      <a href="login.php">Login</a>
+      <a href="register.php">Register</a>
+      <a href="cart.php">Cart</a>
+      <a href="orders.php">Orders</a>
+    </div>
+  </div>
+  <p>Copyright <i class="fa-regular fa-copyright"></i> 2023 <span>Pranjal Ashok Singh | All Rights Reserved.</span></p>
+</section>
